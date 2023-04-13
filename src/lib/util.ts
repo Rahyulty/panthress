23a1001cef2af25d83db695613469e34600d2afe
Rahyulty <ramlalajadhi@gmail.com>
2023-04-12 20:53:31 -0400

    This should fix it

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -33,16 +33,16 @@ export async function getDetaBase() {
 }
 
 
-export function getGreeting(): string {
-  const date = new Date()
-  const hour = date.getHours()
-
-  if (hour < 12){
-    return 'Good Morning!'
-  } else if (hour < 18){
-    return 'Good Afternoon'
-  } else {
-    return 'Good Night'
+export function getGreeting() {
+  const now = new Date();
+  const hour = now.getHours();
+  
+  if (hour >= 6 && hour < 12) {
+    return "Good morning"
+  } else if (hour >= 12 && hour < 18) {
+    return "Good afternoon"
+  } else if (hour >= 18 || hour < 6) {
+    return "Good night"
   }
 }
 
